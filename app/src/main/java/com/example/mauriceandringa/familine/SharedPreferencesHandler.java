931e84a0c53b69b9c381dedcaa9ec12397855810
Maurice Andringa <s1082583@student.windesheim.nl>
2018-11-07 13:01:28 +0100

    p2p connect project

---
+++ app/src/main/java/com/example/mauriceandringa/familine/SharedPreferencesHandler.java
@@ -0,0 +1,27 @@
+package com.example.mauriceandringa.familine;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
+
+public class SharedPreferencesHandler {
+
+    public static SharedPreferences getSharedPreferences(Context ctx) {
+        return PreferenceManager.getDefaultSharedPreferences(ctx);
+    }
+
+    public static void setStringValues(Context ctx, String key,
+                                       String DataToSave) {
+        Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(key, DataToSave);
+        editor.commit();
+    }
+
+    public static String getStringValues(Context ctx, String key) {
+        return getSharedPreferences(ctx).getString(key, null);
+    }
+
+
+}
