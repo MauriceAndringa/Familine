15e51c0095b60011e453c96171eb900ac7410427
Henrik <henrik.dons@windesheim.nl>
2018-11-15 12:46:39 +0100

    add video and voice stream capabilities

---
+++ app/src/main/java/com/example/familine/TurnServer.java
@@ -0,0 +1,11 @@
+package com.example.familine;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.PUT;
+
+public interface TurnServer {
+    @PUT("/_turn/<xyrsys_channel>")
+    Call<TurnServerPojo> getIceCandidates(@Header("Authorization") String authkey);
+}
