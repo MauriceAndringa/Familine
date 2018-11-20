a09e858937aa88b078ae3700eedd6e531bc18b02
Maurice Andringa <s1082583@student.windesheim.nl>
2018-11-20 13:45:15 +0100

    cards layout {hardcoded}

--- app/src/main/java/com/example/mauriceandringa/familine/SplashScreen.java
+++ app/src/main/java/com/example/mauriceandringa/familine/SplashScreen.java
@@ -3,6 +3,7 @@ package com.example.mauriceandringa.familine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity{
 
@@ -10,11 +11,23 @@ public class SplashScreen extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                startMain();
+            }
+        }, 750);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+
+
 
 
     }
+    public void startMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    };
 }
